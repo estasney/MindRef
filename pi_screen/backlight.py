8ed6654542c9950f00db0733bde61d27e8d43193
estasney <estasney@users.noreply.github.com>
2022-02-07 17:03:08 -0500

    Add screen toggling for RPI

--- pi_screen/backlight.py
+++ pi_screen/backlight.py
@@ -4,7 +4,7 @@ import os
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-
+import atexit
 import subprocess as sp
 from datetime import datetime
 from time import sleep
@@ -16,9 +16,9 @@ logger = logging.getLogger(__name__)
 class Backlight(object):
     SCREEN_ON = 0
     SCREEN_OFF = 1
-    BL_DIR = '/sys/devices/platform/rpi_backlight/backlight/rpi_backlight'
-    BL_POWER = os.path.join(BL_DIR, 'bl_power')
-    BL_BRIGHTNESS = os.path.join(BL_DIR, 'brightness')
+    BL_DIR = "/sys/devices/platform/rpi_backlight/backlight/rpi_backlight"
+    BL_POWER = os.path.join(BL_DIR, "bl_power")
+    BL_BRIGHTNESS = os.path.join(BL_DIR, "brightness")
 
     def __init__(self, debug, brightness_steps):
         self.power_state = self.read_state(self.BL_POWER)
@@ -28,7 +28,6 @@ class Backlight(object):
         self.brightness_state = self.read_state(self.BL_BRIGHTNESS)
         self.brightness_step = self.brightness_step_calc(brightness_steps)
         self.debug = debug
-        print(self.max_brightness)
 
     def brightness_step_calc(self, steps):
         # Given the number of steps, find an interval that also satisfies a step being divisible by 5
@@ -38,23 +37,20 @@ class Backlight(object):
         step_size -= step_size_m
         return cycle(list(range(0, mb, step_size)))
 
-
-
     def read_state(self, fp):
-        with open(fp, 'r') as fp:
+        with open(fp, "r") as fp:
             status = int(fp.read())
         return status
 
     def write_state(self, fp, state):
-        p = sp.Popen(['sudo', 'su'], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
-        command = 'echo {} > {}'.format(state, fp).encode()
+        p = sp.Popen(["sudo", "su"], stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+        command = "echo {} > {}".format(state, fp).encode()
         p.stdin.write(command)
         p.communicate()
         return state
 
     def cycle_backlight(self):
         new_value = next(self.brightness_step)
-        print(new_value)
         if new_value == 0:
             self.power_state = self.write_state(self.BL_POWER, self.SCREEN_OFF)
             self.brightness_state = self.write_state(self.BL_BRIGHTNESS, new_value)
@@ -63,6 +59,12 @@ class Backlight(object):
                 self.power_state = self.write_state(self.BL_POWER, self.SCREEN_ON)
             self.brightness_state = self.write_state(self.BL_BRIGHTNESS, new_value)
 
+    def toggle_backlight(self):
+        if self.power_state == self.SCREEN_OFF:
+            self.power_state = self.write_state(self.BL_POWER, self.SCREEN_ON)
+        else:
+            self.power_state = self.write_state(self.BL_POWER, self.SCREEN_OFF)
+
 
 class ScreenButton(object):
     """
@@ -70,27 +72,43 @@ class ScreenButton(object):
     pulldown resistor will read a low state when button is not pressed
     """
 
-    def __init__(self, channel, gpio, debug, brightness_steps):
+    def __init__(self, channel, gpio, debug, brightness_steps, toggle_duration_secs=3):
         self.channel = channel
         self.gpio = gpio
         self.bl = Backlight(debug=debug, brightness_steps=brightness_steps)
         self.debug = debug
+        self.toggle_duration_secs = toggle_duration_secs
+        self.last_pressed = None
         self._setup_pin()
 
     def _setup_pin(self):
         self.gpio.setup(self.channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        self.gpio.add_event_detect(self.channel, self.gpio.RISING, callback=self.button_pushed, bouncetime=1000)
+        self.gpio.add_event_detect(
+            self.channel, self.gpio.RISING, callback=self.button_pushed, bouncetime=250
+        )
+        self.gpio.add_event_detect(
+            self.channel,
+            self.gpio.FALLING,
+            callback=self.button_release,
+            bouncetime=250,
+        )
 
     def button_pushed(self, *args, **kwargs):
-        if self.debug:
-            logger.debug("Button Pushed at {}".format(datetime.now()))
-        self.bl.cycle_backlight()
+        self.last_pressed = datetime.now()
+
+    def button_release(self, *args, **kwargs):
+        if not self.last_pressed:
+            return self.bl.cycle_backlight()
+        duration = datetime.now() - self.last_pressed
+        self.last_pressed = None
+        if duration.total_seconds() >= self.toggle_duration_secs:
+            self.bl.toggle_backlight()
+        else:
+            self.bl.cycle_backlight()
 
 
-if __name__ == '__main__':
-    md = ScreenButton(channel=17, gpio=GPIO, brightness_steps=3, debug=False)
-    try:
-        while True:
-            sleep(60)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
+if __name__ == "__main__":
+    md = ScreenButton(channel=17, gpio=GPIO, brightness_steps=3, debug=False, toggle_duration_secs=3)
+    atexit.register(GPIO.cleanup)
+    while True:
+        sleep(1)
