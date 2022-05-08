38c8a54acc5f965efe3ee9ca5f7de0f06e7fda27
estasney <estasney@users.noreply.github.com>
2022-05-08 00:28:43 -0400

    Enhance test fixture

--- tests/services/conftest.py
+++ tests/services/conftest.py
@@ -1,12 +1,14 @@
 import tempfile
 from pathlib import Path
-
+from typing import Literal
+from string import ascii_letters, digits
 import pytest
 import yaml
+import random
 from PIL import Image, ImageDraw
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def storage_directory():
     temp_dir = tempfile.TemporaryDirectory()
     yield Path(temp_dir.name)
@@ -29,29 +31,65 @@ def stored_categories(storage_directory):
     return storage_directory, data
 
 
+def img_maker(width, height):
+    img = Image.new(mode="RGB", size=(width, height))
+    draw = ImageDraw.Draw(img)
+    draw.line((0, 0, *img.size), fill=128, width=10)
+    return img
+
+
+def static_vars(**kwargs):
+    def wrapped_func(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+
+    return wrapped_func
+
+
 @pytest.fixture()
 def stored_atlas(storage_directory):
-    def _stored_atlas(atlas_name, *img_names):
+    def _stored_atlas(atlas_name, atlas_type: Literal["mono", "multi"], n_images):
+        WIDTH, HEIGHT = 10, 10
+        name_pool = ascii_letters + digits
+
+        @static_vars(seen=set([]))
+        def img_name():
+            while True:
+                img_name_len = random.randint(1, 16)
+                img_letters = random.choices(name_pool, k=img_name_len)
+                img_n = "".join(img_letters)
+                if img_n not in img_name.seen:
+                    img_name.seen.add(img_n)
+                    return img_n
+
         atlas_folder_path = storage_directory / atlas_name
         atlas_folder_path.mkdir()
         img_files = []
-        for im in img_names:
+        img_names = []
+        for i in range(n_images):
+            im = img_name()
             img_path = atlas_folder_path / im
             img_path = img_path.with_suffix(".png")
-            img = Image.new(mode="RGB", size=(24, 24))
-            draw = ImageDraw.Draw(img)
-            draw.line((0, 0, *img.size), fill=128, width=10)
+            img = img_maker(WIDTH, HEIGHT)
             img.save(img_path)
             img_files.append(str(img_path))
+            img_names.append(img_path.stem)
         import os
 
         os.environ["KIVY_NO_ARGS"] = "1"
         from kivy.atlas import Atlas
 
-        Atlas.create(str(atlas_folder_path / atlas_name), img_files, (24 * 4, 24 * 4))
+        # mono atlas can hold all images
+        # multi is split across several
+        if atlas_type == "mono":
+            atlas_size = (WIDTH * (n_images + 1), HEIGHT * (n_images + 1))
+        else:
+            atlas_size = (WIDTH + 2, HEIGHT + 2)
+        Atlas.create(str(atlas_folder_path / atlas_name), img_files, size=atlas_size)
         for im in img_names:
             img_path = atlas_folder_path / im
             img_path.with_suffix(".png").unlink()
-        return atlas_folder_path
+        return atlas_folder_path, img_names
 
     return _stored_atlas
--- tests/services/test_atlas_service.py
+++ tests/services/test_atlas_service.py
@@ -1,22 +1,27 @@
 import json
 
+import pytest
+
 from kvnoteafly.services.atlas.atlas import AtlasService
 
 
-def test_atlas_discovery(stored_atlas):
-    atlas_folder = stored_atlas("test_atlas", *["a", "b", "c"])
+@pytest.mark.parametrize("atlas_type", ["mono", "multi"])
+@pytest.mark.parametrize("n", [1, 5, 20])
+def test_atlas_discovery(stored_atlas, atlas_type, n):
+
+    """
+    Given atlas file, folder and images
+    Check that Atlas Service identifies them and provides uri's correctly
+    """
+    atlas_folder, image_names = stored_atlas("test_atlas", atlas_type, n)
     assert atlas_folder.exists()
     atlas_file = (atlas_folder / "test_atlas").with_suffix(".atlas")
     assert atlas_file.exists()
     with atlas_file.open(mode="r", encoding="utf-8") as fp:
         contents = json.load(fp)
-    img_names = set(
-        item for sublist in [v.keys() for v in contents.values()] for item in sublist
-    )
-    assert img_names == {"a", "b", "c"}
     service = AtlasService(storage_path=atlas_folder.parent)
     assert atlas_file in [atlas.path for atlas in service.atlases]
-    for img in ["a", "b", "c"]:
+    for img in image_names:
         assert (
             service.uri_for(img, "test_atlas")
             == f"atlas://{atlas_file.with_suffix('')}/{img}"
