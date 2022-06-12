8c005165e68a830a1b7f800a4793ebea82d84069
estasney <estasney@users.noreply.github.com>
2022-06-11 23:46:10 -0400

    utils for generating textures

--- scripts/make_texture.py
+++ scripts/make_texture.py
@@ -1,28 +1,35 @@
+from pathlib import Path
+from typing import Sequence
+
 from PIL import Image, ImageDraw
 from colour import Color
+from collections import namedtuple
 
 c1 = Color("#6d7272")
 c2 = Color("#388fe5")
 
+TextureParam = namedtuple("TextureParam", "main, accent, steps, size")
+ImgBox = namedtuple("ImgBox", "x,y,w,h")
 
-def gradient_texture(tx_size, color_main: Color, color_accent: Color, n_steps: int):
+
+def gradient_texture(main: Color, accent: Color, steps: int, size):
     """
     Generate 9-scaling texture image
 
     Parameters
     ----------
-    tx_size: int
+    size: int
         (width/height) of image. Assumed to be square
-    color_main:
-    color_accent:
-    n_steps:
+    main:
+    accent:
+    steps:
     """
-    c_steps = list(color_accent.range_to(color_main, n_steps))
-    img = Image.new(mode="RGB", size=(tx_size, tx_size))
+    c_steps = list(accent.range_to(main, steps))
+    img = Image.new(mode="RGB", size=(size, size))
     draw = ImageDraw.Draw(img)
 
     px0 = 0
-    px1 = tx_size - 1
+    px1 = size - 1
 
     for c in c_steps:
         draw.rectangle((px0, px0, px1, px1), fill=c.hex_l)
@@ -33,12 +40,16 @@ def gradient_texture(tx_size, color_main: Color, color_accent: Color, n_steps: i
 
 
 if __name__ == "__main__":
-    btn_press = [Color("#4a99e8"), Color("#388fe5")]
-    btn_normal = [Color("#37464f"), Color("#101f27")]
+    btn_down = TextureParam(
+        main=Color("#4a99e8"), accent=Color("#101f27"), steps=15, size=64
+    )
+    btn_normal = TextureParam(
+        main=Color("#37464f"), accent=Color("#101f27"), steps=5, size=64
+    )
 
-    gradient_texture(64, *btn_press, n_steps=5).save(
-        "../kvnoteafly/static/textures/bg_down.png"
-    )
-    gradient_texture(64, *btn_normal, n_steps=5).save(
-        "../kvnoteafly/static/textures/bg_normal.png"
-    )
+    texture_path = Path(__file__).parent.parent / "kvnoteafly" / "static" / "textures"
+    for file in texture_path.iterdir():
+        file.unlink()
+
+    gradient_texture(**btn_down._asdict()).save(texture_path / "bg_down.png")
+    gradient_texture(**btn_normal._asdict()).save(texture_path / "bg_normal.png")
