85fd00b77acf62364e6c48db4da26ba2bb39c64f
estasney <estasney@users.noreply.github.com>
2022-08-06 00:35:30 -0400

    Resolves #3
    Flicker was caused by complete removal of category buttons on each update

--- kvnoteafly/widgets/categories.py
+++ kvnoteafly/widgets/categories.py
@@ -48,24 +48,34 @@ class NoteCategories(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         fbind = self.fbind
-        fbind("categories", self.handle_categories)
+        self.draw_categories_trigger = Clock.create_trigger(self.draw_categories)
+        fbind("categories", self.draw_categories_trigger)
 
     def category_callback(self, instance: "NoteCategoryButton"):
         self.parent.category_selected(instance)
 
-    def handle_categories(self, instance, value):
-        Clock.schedule_once(
-            partial(self.clear_categories, callback=self.draw_categories)
-        )
-
-    def clear_categories(self, dt, callback):
-        self.category_container.clear_widgets()
-        if callback:
-            Clock.schedule_once(callback)
-
     def draw_categories(self, dt):
-        for category in self.categories:
-            cat_btn = NoteCategoryButton(text=category)
+
+        container = self.category_container
+
+        screen_categories = set((child.text for child in container.children))
+        prop_categories = set(self.categories)
+
+        remove_categories = screen_categories - prop_categories
+        add_categories = prop_categories - screen_categories
+
+        # Removal
+        for r_cat in remove_categories:
+            matched_btn = next(
+                (child for child in container.children if child.text == r_cat), None
+            )
+            if not matched_btn:
+                continue
+            container.remove_widget(matched_btn)
+
+        # Addition
+        for a_cat in add_categories:
+            cat_btn = NoteCategoryButton(text=a_cat)
             cat_btn.bind(on_release=self.category_callback)
             self.category_container.add_widget(cat_btn)
 
