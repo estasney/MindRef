7ce6b0097b26a7b12b3936080e834e77253ced9c
estasney <estasney@users.noreply.github.com>
2022-08-14 01:07:45 -0400

    fix shader for glowline

--- mindref/widgets/effects/pulsing.py
+++ mindref/widgets/effects/pulsing.py
@@ -1,3 +1,4 @@
+from kivy.base import EventLoop
 from kivy.uix.effectwidget import EffectBase
 
 effect_pulsing = """
@@ -11,17 +12,19 @@ float cubicPulse( float c, float w, float x ){
 }
 
 vec4 effect(vec4 color, sampler2D texture, vec2 text_coords, vec2 coords) {
-             // increase d to balance pulsed vs un-pulsed spread
-             float d = 0.8;
-             vec2 st = coords.xy/resolution;
-             float y = cubicPulse(0.5,max(abs(sin(time)),0.6),st.x);
-             vec3 cl = vec3(y);
-             return vec4(cl,1.0);
-         }
+    float d = 0.8;
+    vec2 st = coords.xy/resolution;
+    float y = cubicPulse(0.5, max(abs(sin(time)), 0.6), st.x);
+    vec4 cl = vec4(y, y, y, 1.0);
+    // This is required to avoid Android crashing
+    return (texture2D (texture, text_coords) * cl);
+}
+
 """
 
 
 class PulsingEffect(EffectBase):
     def __init__(self, *args, **kwargs):
+        EventLoop.ensure_window()
         super(PulsingEffect, self).__init__(*args, **kwargs)
         self.glsl = effect_pulsing
