0e0d199bd3eb04c53361e9204033dd010381fde8
estasney <estasney@users.noreply.github.com>
2022-11-22 00:02:04 -0500

    Move state for note

--- mindref/widgets/note.py
+++ mindref/widgets/note.py
@@ -1,13 +1,24 @@
-from copy import deepcopy
 from typing import TYPE_CHECKING
 
 from kivy import Logger
 from kivy.app import App
 from kivy.cache import Cache
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import (
+    DictProperty,
+    NumericProperty,
+    ObjectProperty,
+    StringProperty,
+)
 from kivy.uix.boxlayout import BoxLayout
 
-from utils import import_kv, sch_cb
+from domain.events import (
+    AddNoteEvent,
+    BackButtonEvent,
+    EditNoteEvent,
+    ListViewButtonEvent,
+    PaginationEvent,
+)
+from utils import import_kv
 from utils.caching import cache_key_note, kivy_cache
 from widgets.behavior.gesture_rec_behavior import GestureRecognizingBehavior
 
@@ -17,18 +28,21 @@ from widgets.markdown.markdown_document import MarkdownDocument
 
 if TYPE_CHECKING:
     from domain.markdown_note import MarkdownNoteDict
+    from typing import Optional
 
 Cache.register("note_widget", limit=10, timeout=3600)
 
 
+# noinspection PyUnusedLocal
 @kivy_cache(cache_name="note_widget", key_func=cache_key_note)
 def get_cached_note(*, content_data: "MarkdownNoteDict", parent: "NoteContent"):
     return MarkdownDocument(content_data=content_data)
 
 
 class Note(BoxLayout, GestureRecognizingBehavior):
-    note_title = ObjectProperty()
-    note_content = ObjectProperty()
+    note_title = StringProperty()
+    note_index = NumericProperty()
+    note_content = DictProperty()
     note_control = ObjectProperty()
 
     def __init__(self, **kwargs):
@@ -38,36 +52,27 @@ class Note(BoxLayout, GestureRecognizingBehavior):
     def handle_swipe(self, _instance, _score, gesture):
         app = App.get_running_app()
         if gesture.name == "swipe-left":
-            app.paginate(-1)
+            app.registry.push_event(PaginationEvent(direction=-1))
         elif gesture.name == "swipe-right":
-            app.paginate(1)
+            app.registry.push_event(PaginationEvent(direction=1))
         return True
 
-    def set_note_content(self, note_data: "MarkdownNoteDict"):
-        title = note_data["title"]
-        set_title = lambda x: self.note_title.set({"title": title})
-        data = deepcopy(note_data)
-        set_content = lambda x: self.note_content.set(data)
-        sch_cb(set_title, set_content)
-
-    def clear_note_content(self):
-        clear_title = lambda x: self.note_title.set({"title": ""})
-        self.note_title.set({"title": ""})
-        clear_content = lambda x: self.note_content.clear()
-        sch_cb(clear_title, clear_content)
+    def set_note_content(self, note_data: "Optional[MarkdownNoteDict]"):
+        self.note_title = note_data.get("title", "")
+        self.note_index = note_data.get("idx", -1)
+        self.note_content = note_data if note_data else {}
 
 
 class NoteContent(BoxLayout):
-    def set(self, content_data: "MarkdownNoteDict"):
-        self.clear_widgets()
-        Logger.debug(
-            f"NoteContent: {content_data['category']}, {content_data['title']}"
-        )
+    content = DictProperty()
 
+    def __init__(self, **kwargs):
+        super(NoteContent, self).__init__(**kwargs)
+        self.bind(content=self.handle_content)
+
+    def handle_content(self, _, content_data: "MarkdownNoteDict"):
+        self.clear_widgets()
         self._set_markdown(content_data)
-
-    def clear(self):
-        self.clear_widgets()
 
     def _set_markdown(self, content_data: "MarkdownNoteDict"):
         md_widget = get_cached_note(content_data=content_data, parent=self)
@@ -75,19 +80,38 @@ class NoteContent(BoxLayout):
 
 
 class NoteTitleBar(BoxLayout):
-    title_text = StringProperty()
-    edit_button = ObjectProperty()
+    title = StringProperty()
+    index = NumericProperty()
+    action_button = ObjectProperty()
 
     def __init__(self, **kwargs):
-        if "note_title" in kwargs:
-            self.title_text = kwargs.pop("note_title")
         super().__init__(**kwargs)
+        self.bind(action_button=self.handle_button_bind)
 
     def handle_button_bind(self, *_args):
         self.action_button.bind(on_select=self.handle_select)
 
-    def handle_edit(self):
-        """Pass this to screen manager"""
-        Logger.debug("Edit")
-        self.parent.handle_edit()
-        return True
+    def handle_select(self, _, value):
+
+        app = App.get_running_app()
+        match value:
+            case "add":
+                app.registry.push_event(AddNoteEvent())
+                return True
+            case "edit":
+                app.registry.push_event(
+                    EditNoteEvent(category=app.note_category, idx=self.index)
+                )
+                return True
+            case "list":
+                app.registry.push_event(ListViewButtonEvent())
+                return True
+            case "back":
+                app.registry.push_event(
+                    BackButtonEvent(current_display_state="display")
+                )
+                return True
+            case _:
+                Logger.warning(
+                    f"{self.__class__.__name__}: handle_select - Unhandled value {value}"
+                )
