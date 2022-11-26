5aca3cd9bfaec406b1cba195ff21cf1d5913c293
estasney <estasney@users.noreply.github.com>
2022-11-26 00:02:53 -0500

    adapt test_category_discovery with new callback structure and CategoryResourceFiles Test for return type to be None on empty query Add NoteRepository.index testing Update monkey patch for test_query_notes. Add assert helper and remove whitespace characters from title_generator

--- tests/adapters/notes/fs/test_fs_note_repo.py
+++ tests/adapters/notes/fs/test_fs_note_repo.py
@@ -1,7 +1,13 @@
-from functools import reduce
-from itertools import combinations, product
+import random
+import string
+from functools import partial, reduce
+from itertools import product
 from operator import and_, attrgetter
-from typing import Callable, Optional
+from typing import Callable, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from domain.note_resource import CategoryResourceFiles
+    from widgets.typeahead.typeahead_dropdown import Suggestion
 
 import pytest
 from toolz import sliding_window
@@ -12,16 +18,56 @@ from pathlib import Path
 from domain.markdown_note import MarkdownNote
 
 
+@pytest.fixture
+def app_registry():
+    registry = type("registry", (object,), {"push_event": lambda x: x})
+    return type("FakeApp", (object,), {"registry": registry})
+
+
+@pytest.fixture
+def note_repo_factory(
+    filesystem_data, app_registry
+) -> Callable[..., "FileSystemNoteRepository"]:
+    def note_repo_(n_notes: int, category_selected: bool) -> "FileSystemNoteRepository":
+        category_files, root_folder = filesystem_data(1, n_notes)
+        fs = FileSystemNoteRepository(new_first=True, get_app=lambda: app_registry)
+        fs.storage_path = root_folder
+        fs.discover_categories(lambda x: x)
+        if category_selected:
+            cat = next((k for k in category_files.keys()), None)
+            fs.current_category = cat.name
+
+        return fs
+
+    r = note_repo_
+
+    return note_repo_
+
+
 @pytest.mark.parametrize("n_categories", [0, 1, 2, 3])
-def test_category_discovery(n_categories, category_folders):
+def test_category_discovery(n_categories, category_folders, app_registry):
+    """
+    Given directory with 'category' folders
+    Call FileSystemNoteRepository.get_categories
+    Check that categories and their images are discovered
+    """
     expected_folders, root_folder = category_folders(n_categories)
-    fs = FileSystemNoteRepository(new_first=True, get_app=lambda: None)
+    fs = FileSystemNoteRepository(new_first=True, get_app=lambda: app_registry)
     fs.storage_path = root_folder
-    discovery = list(fs.discover_notes())
-    for folder, img in expected_folders:
-        assert folder.name in fs.categories
-        assert folder.name in ((d.category for d in discovery))
-        assert img in ((d.image_path for d in discovery))
+
+    def fs_get_categories_callback(cat):
+        assert set(cat) == set(folder.name for folder, _ in expected_folders)
+
+    fs.get_categories(fs_get_categories_callback)
+
+    # Test for category discovery
+    def fs_discovery_callback(resource: "CategoryResourceFiles", category_name, img):
+        assert resource.category == category_name
+        assert resource.image.path == img
+
+    for name, img in ((folder.name, img) for folder, img in expected_folders):
+        cb = partial(fs_discovery_callback, category_name=name, img=img)
+        fs.discover_category(name, cb)
 
 
 @pytest.fixture()
@@ -35,44 +81,90 @@ def notes():
 
 @pytest.fixture()
 def title_query_generator():
+    """
+    Generate a query that meets the condition of matching matched notes and not matching unmatched notes
+
+    Returns
+    -------
+
+    """
+
     def title_query_generator_(
         matched: list[MarkdownNote], unmatched: Optional[list[MarkdownNote]]
     ):
         get_title: Callable[[MarkdownNote], str] = attrgetter("title")
-        matched_titles = [get_title(n).lower() for n in matched]
+        matched_titles = [get_title(n).lower() for n in matched] if matched else None
         unmatched_titles = (
             [get_title(n).lower() for n in unmatched] if unmatched else None
         )
 
         def query_gen(grp: list[str]):
             # Generate queries that will match the grp
-            common_chars = reduce(and_, (set(t) for t in grp))
+            common_chars = reduce(
+                and_,
+                (
+                    set((char for char in t if char not in string.whitespace))
+                    for t in grp
+                ),
+            )
             if len(common_chars) < 3:
                 raise AssertionError(
                     f"{', '.join(grp)} only share {len(common_chars)}, {','.join(sorted(common_chars))}"
                 )
 
             # Use sliding windows and generators to generate queries that match the grp
-            chunks = product(*(sliding_window(3, t) for t in grp))
+            chunks = product(
+                *(
+                    sliding_window(
+                        3, (char for char in t if char not in string.whitespace)
+                    )
+                    for t in grp
+                )
+            )
             for chunk in chunks:
                 sc = set(("".join(chars) for chars in chunk))
                 if len(sc) == 1:
                     yield "".join(chunk[0])
 
         def query_pipeline():
-            matched_title_gen = query_gen(matched_titles)
-            if not unmatched_titles:
-                return next(matched_title_gen)
-            for query in matched_title_gen:
-                if not any((query in ut for ut in unmatched_titles)):
-                    return query
+            match (matched_titles, unmatched_titles):
+                case list(), None:
+                    matched_title_gen = query_gen(matched_titles)
+                    return next(matched_title_gen)
+                case list(), list():
+                    matched_title_gen = query_gen(matched_titles)
+                    for query in matched_title_gen:
+                        if not any((query in ut for ut in unmatched_titles)):
+                            return query
+                case None, list() | None:
+                    # Generate random sequences
+                    make_chars = (
+                        "".join(random.choices(string.ascii_lowercase, k=3))
+                        for _ in range(10_000)
+                    )
+                    for query in make_chars:
+                        if unmatched_titles:
+                            if not any((query in ut for ut in unmatched_titles)):
+                                return query
+                        else:
+                            return query
 
         return query_pipeline()
 
     return title_query_generator_
 
 
-@pytest.mark.parametrize("matching", [(slice(2), None)])
+def matches_title_or_text(query, suggestion: "Suggestion", notes):
+    matches_titles = suggestion.title in (n.title for n in notes)
+    matches_text = any(n.text.find(query) for n in notes)
+    return matches_text or matches_titles
+
+
+@pytest.mark.parametrize(
+    "matching",
+    [(slice(1), None), (slice(2), None), (slice(2), slice(2, 3)), (None, None)],
+    ids=lambda x: f"Matched: {x[0]}, Unmatched: {x[1]}",
+)
 def test_query_notes(matching, notes, title_query_generator, monkeypatch):
     """
     - Given a collection of notes, and a query
@@ -80,7 +172,7 @@ def test_query_notes(matching, notes, title_query_generator, monkeypatch):
     - Check for false positives, false negatives and ranking
     """
     match_idx, no_match_idx = matching
-    match_notes = notes[match_idx]
+    match_notes = notes[match_idx] if match_idx else None
     unmatched_notes = notes[no_match_idx] if no_match_idx else None
     q = title_query_generator(match_notes, unmatched_notes)
     fs = FileSystemNoteRepository(new_first=True, get_app=lambda: None)
@@ -88,9 +180,65 @@ def test_query_notes(matching, notes, title_query_generator, monkeypatch):
     def patch_meta(*args, **kwargs):
         return [note.to_dict() for note in notes]
 
-    monkeypatch.setattr(fs, "_load_category_meta", patch_meta)
-    result = fs.query_notes("test", q, None)
+    monkeypatch.setattr(fs, "_get_category_meta", patch_meta)
+
+    result = fs.query_notes(category="test", query=q, on_complete=None)
+    if not match_notes and not unmatched_notes:
+        assert result is None
+        return
     for suggestion in result:
-        assert suggestion.title in (n.title for n in match_notes)
+        assert matches_title_or_text(q, suggestion, match_notes)
         if unmatched_notes:
-            assert suggestion.title not in (n.title for n in unmatched_notes)
+            assert suggestion.title not in (note.title for note in unmatched_notes)
+
+
+@pytest.mark.parametrize("n_notes", [0, 10])
+@pytest.mark.parametrize("cat_selected", [True, False])
+def test_index_sizing(n_notes, cat_selected, note_repo_factory):
+    """
+    Given NoteRepository
+    With different categories, including None,
+    Check that index size is correct
+    Returns
+    -------
+
+    """
+    fs = note_repo_factory(n_notes=n_notes, category_selected=cat_selected)
+    if not cat_selected:
+        assert fs.current_category is None
+        with pytest.raises(Exception, match="No Index"):
+            fs.index_size()
+        with pytest.raises(Exception, match="No Index"):
+            _ = fs.index
+        with pytest.raises(Exception, match="No Index"):
+            fs.set_index(0)
+        return
+    assert fs.index_size() == n_notes
+    # Ensure index is cleared when category switches to None
+    fs_category = fs.current_category
+    fs.current_category = None
+    with pytest.raises(Exception, match="No Index"):
+        fs.index_size()
+    with pytest.raises(Exception, match="No Index"):
+        _ = fs.index
+    with pytest.raises(Exception, match="No Index"):
+        fs.set_index(0)
+    fs.current_category = fs_category
+    assert fs.index
+
+
+@pytest.mark.parametrize("n_notes", [0, 10])
+@pytest.mark.parametrize("cat_selected", [True, False])
+def test_get_note(n_notes, cat_selected, note_repo_factory):
+    """
+    Given NoteRepository
+    Ask To Get a Note By Index
+    Check that a valid idx receives a 'MarkdownNote' and Invalid raises
+    """
+
+    fs = note_repo_factory(n_notes=n_notes, category_selected=cat_selected)
+    if n_notes == 0:
+        with pytest.raises(Exception):
+            fs.get_note("0", 0, None)
+    else:
+        assert type(fs.get_note("0", 0, None)) == MarkdownNote
