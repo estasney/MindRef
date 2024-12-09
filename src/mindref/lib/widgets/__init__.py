9f09f8f220a65f86ee7d445e373f37942e15b3a2
estasney <estasney@users.noreply.github.com>
2024-12-08 19:35:40 -0500

    Refactoring imports

--- src/mindref/lib/widgets/__init__.py
+++ src/mindref/lib/widgets/__init__.py
@@ -1,9 +1,12 @@
+# Standalone
 from .separator import (
     AnimatedHSeparator,
     HSeparator,
     Separator,
     VSeparator,
 )
+
+# Requires InteractBehavior
 from .settings_panel import (
     AndroidSettingPath,
     MindRefSettingsAndroid,
---
+++ src/mindref/lib/widgets/behavior/__init__.py
@@ -0,0 +1,3 @@
+from .inline_behavior import LabelHighlightInline
+from .interact_behavior import InteractBehavior
+from .refresh_behavior import RefreshBehavior
