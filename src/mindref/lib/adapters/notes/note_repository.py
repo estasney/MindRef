cfc565a51114d6f3206c072860630d74630a8cab
estasney <estasney@users.noreply.github.com>
2025-07-13 12:15:35 -0400

    Update type checking rules and add extra paths for stubs

--- src/mindref/lib/adapters/notes/note_repository.py
+++ src/mindref/lib/adapters/notes/note_repository.py
@@ -11,12 +11,9 @@ from typing import (
 if TYPE_CHECKING:
     from collections.abc import Callable
     from os import PathLike
-    from pathlib import Path
 
     from mindref.lib.domain.editable import EditableNote
-    from mindref.lib.domain.markdown_note import MarkdownNote
     from mindref.lib.domain.protocols import GetApp
-    from mindref.lib.ext import RollingIndex
 
     from ...domain.settings import SortOptions
     from .android.android_note_repository import AndroidNoteRepository
@@ -30,10 +27,6 @@ class NoteRepositoryInitProtocol(Protocol):
     def __init__(
         self,
         get_app: GetApp,
-        note_sorting: SortOptions,
-        note_sorting_ascending: bool,
-        category_sorting: SortOptions,
-        category_sorting_ascending: bool,
         **kwargs,
     ): ...
 
@@ -66,7 +59,7 @@ class NoteRepositoryFactory:
                 return cls._get_repo_default()
 
 
-class AbstractNoteRepository(ABC):
+class AbstractNoteRepository(ABC, NoteRepositoryInitProtocol):
     @property
     @abc.abstractmethod
     def configured(self) -> bool:
@@ -81,61 +74,8 @@ class AbstractNoteRepository(ABC):
     def storage_path(self, path: PathLike | None):
         raise NotImplementedError
 
-    def get_categories(self, on_complete: Callable) -> list[str]:
-        raise NotImplementedError
-
-    @property
-    @abc.abstractmethod
-    def current_category(self):
-        raise NotImplementedError
-
-    @current_category.setter
-    @abc.abstractmethod
-    def current_category(self, value: str):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def create_category(self, name: str, image_path: Path | str, on_complete: Callable):
-        raise NotImplementedError
-
-    def get_category_meta(
-        self, category: str, on_complete: Callable, refresh: bool = False
-    ):
-        """For self.current_category, get MarkdownNoteDict for all files in category
-
-        Parameters
-        ----------
-        category
-        on_complete
-        refresh : bool
-            If False, (default) don't force-reload of MarkdownNotes
-            If True, force-reload
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def discover_categories(self, on_complete: Callable) -> None:
-        """Discover all categories and emit a DiscoverCategoryEvent"""
-        raise NotImplementedError
-
     @abc.abstractmethod
     def discover_notes(self, on_complete: Callable) -> None:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_next_note(self, on_complete: Callable | None) -> MarkdownNote:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_previous_note(self, on_complete: Callable | None) -> MarkdownNote:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_current_note(self, on_complete: Callable | None):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def get_note(self, category: str, idx: int, on_complete: Callable | None):
         raise NotImplementedError
 
     @abc.abstractmethod
@@ -144,45 +84,4 @@ class AbstractNoteRepository(ABC):
         note: EditableNote,
         on_complete: Callable,
     ):
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def set_index(self, n: int | None):
-        raise NotImplementedError
-
-    @property
-    @abc.abstractmethod
-    def index(self) -> RollingIndex:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def index_size(self) -> int:
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def category_image_uri(self, category: str):
-        """Return URI for a Category Image"""
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def query_notes(
-        self,
-        category: str,
-        query: str,
-        on_complete: Callable | None,
-    ):
-        """Search Notes
-
-        Parameters
-        ----------
-        category: str
-        query: str
-            String query to search
-        on_complete
-        """
-        raise NotImplementedError
-
-    @abc.abstractmethod
-    def category_name_unique(self, category: str) -> bool:
-        """Return True if category name is unique"""
         raise NotImplementedError
--- src/mindref/main.py
+++ src/mindref/main.py
@@ -7,33 +7,7 @@ from kivy.core.text import LabelBase
 
 
 def run_android():
-    import shutil
-
-    def bootstrap_c_build():
-        # Copy the mindref compiled .so file(s) from the python-for-android build which will be located in the site-packages directory
-        # This is architecture dependent but at runtime, the architecture specific site-packages directory will be used automatically
-        site_packages = Path(__file__).parent / "_python_bundle" / "site-packages"
-
-        app_lib = Path(__file__).parent / "lib"
-        app_lib_ext = app_lib / "ext"
-
-        lib_ext = "ext.so"
-        lib_ext_src = site_packages / "mindref_ext" / lib_ext
-        lib_ext_dest = app_lib_ext / lib_ext
-
-        def copy_lib(src, dest):
-            if not dest.exists():
-                Logger.info(f"Copying {src} to {dest}")
-                shutil.copy(src, dest)
-            else:
-                Logger.info(f"{dest} exists")
-
-        # copy_lib(lib_ext_src, lib_ext_dest)
-
-    # bootstrap_c_build()
-
     Logger.info("Running Android")
-
     from mindref.app import MindRefApp
 
     app = MindRefApp()
