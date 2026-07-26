6f633c072991e1cf4ca0b45b43f4ce866231c10a
estasney <estasney@users.noreply.github.com>
2026-07-25 21:10:32 -0400

    Additional type narrowing

--- src/mindref/lib/domain/editable.py
+++ src/mindref/lib/domain/editable.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
+from mindref.lib.utils import required
+
 if TYPE_CHECKING:
     from mindref.lib.domain.markdown_note import MarkdownNote
 
@@ -20,11 +22,12 @@ class EditableNote:
         return f"{type(self).__name__}({','.join(f'{p}={getattr(self, p)}' for p in attrs)})"
 
     @classmethod
-    def from_markdown_note(cls, note: MarkdownNote):
+    def from_markdown_note(cls, note: MarkdownNote) -> EditableNote:
+        fp = required(note.filepath, "Note's filepath should not be None")
         return EditableNote(
             category=note.category,
             idx=note.idx,
             md_note=note,
             edit_text=note.text,
-            edit_title=note.filepath.stem,
+            edit_title=fp.stem,
         )
