649084cfc7e197229be5a47c92d0cc6bdaaa6b1b
estasney <estasney@users.noreply.github.com>
2026-07-29 19:10:10 -0400

    Extend typing

--- src/mindref/lib/widgets/nav_drawer/nav_item.py
+++ src/mindref/lib/widgets/nav_drawer/nav_item.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from functools import partial
 
@@ -6,11 +8,19 @@ from kivy.clock import Clock
 from kivy.graphics import Color, RoundedRectangle
 from kivy.lang import Builder
 from kivy.metrics import dp
-from kivy.properties import BooleanProperty, ColorProperty, StringProperty
+from kivy.properties import (
+    BooleanProperty,
+    ColorProperty,
+    NumericProperty,
+    OptionProperty,
+    StringProperty,
+)
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.widget import Widget
 
 from mindref.app_notes import NoteFile
+from mindref.lib.models import AnimationTiming
+from mindref.lib.utils import required
 from mindref.lib.widgets.behavior import DebugBoxLayout
 
 Builder.load_string("""
@@ -36,48 +46,69 @@ Builder.load_string("""
 class SelectableAnimation(Widget):
     selected = BooleanProperty(False)
     bg_color_selected = ColorProperty()
+    bg_color_unselected = ColorProperty((0, 0, 0, 0))
+    canvas_rect_segments = NumericProperty(50)
+    canvas_ready_retry_delay = NumericProperty(0.1)
+    selected_inset_x = NumericProperty(dp(12))
+    animation_select_duration = NumericProperty(0.1)
+    animation_select_timing: OptionProperty[AnimationTiming] = OptionProperty(
+        AnimationTiming.in_quad, options=list(AnimationTiming)
+    )
+    animation_deselect_duration = NumericProperty(0.2)
+    animation_deselect_timing: OptionProperty[AnimationTiming] = OptionProperty(
+        AnimationTiming.in_out_quad, options=list(AnimationTiming)
+    )
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: object):
         self._select_anim = None
         self.canvas_rect_color = None
         self.canvas_rect = None
         super().__init__(**kwargs)
         with self.canvas:
-            self.canvas_rect_color = Color(rgba=(0, 0, 0, 0))
+            self.canvas_rect_color = Color(rgba=self.bg_color_unselected)
             self.canvas_rect = RoundedRectangle(
-                pos=self.pos, size=self.size, radius=[0, 0, 0, 0], segments=50
+                pos=self.pos,
+                size=self.size,
+                radius=[0, 0, 0, 0],
+                segments=int(self.canvas_rect_segments),
             )
         self.bind(pos=self._update_canvas, size=self._update_canvas)
 
-    def _update_canvas(self, *_args):
+    def _update_canvas(self, *_args: object) -> None:
         if self.canvas_rect:
             self.canvas_rect.pos = self.pos
             self.canvas_rect.size = self.size
 
-    def on_selected(self, _, is_selected: bool):
+    def on_selected(self, _instance: object, is_selected: bool) -> None:
         if not self.canvas_rect:  # We have to reschedule this once canvas is ready
             cb = partial(self.on_selected, is_selected=is_selected)
-            Clock.schedule_once(cb, 0.1)
+            Clock.schedule_once(cb, self.canvas_ready_retry_delay)
             return
+
+        rect_color = required(self.canvas_rect_color, "canvas_rect_color not set")
 
         if is_selected:
             if self._select_anim:
                 self._select_anim.stop(self.canvas_rect)
             self.canvas_rect.radius = [self.height / 2] * 4
             self._select_anim = Animation(
-                pos=(self.x + dp(12), self.y),
-                size=(self.width - dp(24), self.height),
-                duration=0.1,
-                t="in_quad",
+                pos=(self.x + self.selected_inset_x, self.y),
+                size=(self.width - (2 * self.selected_inset_x), self.height),
+                duration=self.animation_select_duration,
+                t=self.animation_select_timing,
             )
             self._select_anim.start(self.canvas_rect)
-            self.canvas_rect_color.rgba = self.bg_color_selected
+            rect_color.rgba = self.bg_color_selected
         else:
             if self._select_anim:
                 self._select_anim.stop(self.canvas_rect)
-            self._select_anim = Animation(size=(0, 0), duration=0.2, t="in_out_quad")
+            self._select_anim = Animation(
+                size=(0, 0),
+                duration=self.animation_deselect_duration,
+                t=self.animation_deselect_timing,
+            )
             self._select_anim.start(self.canvas_rect)
-            self.canvas_rect_color.rgba = (0, 0, 0, 0)
+            rect_color.rgba = self.bg_color_unselected
 
 
 @dataclass()
@@ -87,7 +118,7 @@ class NavItemData:
     selected: bool
 
     @classmethod
-    def from_note_file(cls, data: "NoteFile", selected: bool) -> "NavItemData":
+    def from_note_file(cls, data: NoteFile, selected: bool) -> NavItemData:
         return cls(display_name=data.label, nav_id=data.id, selected=selected)
 
 
@@ -97,7 +128,7 @@ class NavItem(ButtonBehavior, DebugBoxLayout, SelectableAnimation):
     selected = BooleanProperty(False)
     bg_color_selected = ColorProperty()
 
-    def __init__(self, text: str, nav_id: str, selected: bool, **kwargs):
+    def __init__(self, text: str, nav_id: str, selected: bool, **kwargs: object):
         super().__init__(**kwargs)
         self.text = text
         self.nav_id = nav_id
