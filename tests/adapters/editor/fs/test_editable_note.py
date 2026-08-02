63e9cb586987d4678560340915f4e79e55918fb4
estasney <estasney@users.noreply.github.com>
2026-08-02 15:13:13 -0400

    Remove tests

--- tests/adapters/editor/fs/test_editable_note.py
+++
@@ -1,6 +0,0 @@
-from mindref.lib.domain.editable import EditableNote
-
-
-def test_editable_note_post_init(md_note):
-    edit_note = EditableNote.from_markdown_note(md_note)
-    assert edit_note.edit_text == md_note.text
